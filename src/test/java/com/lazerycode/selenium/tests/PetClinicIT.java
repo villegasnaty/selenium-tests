a2305ba3f8f03e51844ecae9f1f3ea8ea635863d
geraving@gmail.com <geraving@gmail.com>
2018-12-20 18:03:44 -0300

    modifique test

--- src/test/java/com/lazerycode/selenium/tests/PetClinicIT.java
+++ src/test/java/com/lazerycode/selenium/tests/PetClinicIT.java
@@ -11,14 +11,45 @@ import com.lazerycode.selenium.DriverBase;
 public class PetClinicIT extends DriverBase {
 	
 	@Test
-    public void petClinic() throws Exception {
+    public void petClinicOwners() throws Exception {
         WebDriver driver = getDriver();
         Actions action = new Actions(driver);
         WebDriverWait wait = new WebDriverWait(driver, 10, 100);
 
         driver.navigate().to("http://localhost:8081");
+        
 
         driver.findElement(By.partialLinkText("FIND OWNERS")).click();
-        wait.until(d -> d.getCurrentUrl().startsWith("http://localhost:8081/owners/find"));
+        driver.findElement(By.linkText("Add Owner")).click();
+        driver.findElement(By.id("firstName")).sendKeys("Martin");
+		driver.findElement(By.id("lastName")).sendKeys("Torres");
+		driver.findElement(By.id("address")).sendKeys("san juan 9");
+		driver.findElement(By.id("city")).sendKeys("Cordoba");
+		driver.findElement(By.id("telephone")).sendKeys("3516789010");
+		
+		
+		
+		driver.findElement(By.xpath("//button")).click();
+		
+		driver.findElement(By.partialLinkText("Find owners")).click();
+		driver.findElement(By.id("lastName")).sendKeys("Torres");
+		driver.findElement(By.xpath("//button")).click();
+	
+		wait.until(d -> d.getCurrentUrl().startsWith("http://localhost:8081/owners"));
     }
+	
+	@Test
+    public void petClinicVeterinarians() throws Exception {
+        WebDriver driver = getDriver();
+        Actions action = new Actions(driver);
+        WebDriverWait wait = new WebDriverWait(driver, 10, 100);
+
+       driver.navigate().to("http://localhost:8081");
+       
+       driver.findElement(By.partialLinkText("VETERINARIANS")).click();
+       
+       wait.until(d -> d.getCurrentUrl().startsWith("http://localhost:8081/vets.html"));
+       
+	}
+	
 }
